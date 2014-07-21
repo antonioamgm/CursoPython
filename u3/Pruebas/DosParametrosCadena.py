b91b1ff47abcd24ad8c12bce7a814bb301ad7f4c
antonioam <antonioamgm178@gmail.com>
2014-07-21 17:41:58 +0200

    Cambiado de nombre AAM

--- u3/Pruebas/DosParametrosCadena.py
+++ u3/Pruebas/DosParametrosCadena.py
@@ -4,5 +4,3 @@ if nombre2=="Antonio" and nombre1=="José":
     print("El nombre1 vale %s y el nombre2 vale %s." %(nombre1, nombre2))
     print("Esta linea también saldrá junto con el if si el nombre es Antonio")
 print("Esta cadena saldrá siempre.")
-
-
