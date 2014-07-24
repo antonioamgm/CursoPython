efe69c488c2ce7cc1511b3b56d806c3be0d2baa1
antonioam <antonioamgm178@gmail.com>
2014-07-24 20:35:35 +0200

    Pruebas y fallos

--- u7/Usuarios/Interfact.py
+++ u7/Usuarios/Interfact.py
@@ -1,22 +1,34 @@
 import sqlite3 as dbapi
 
+import Usuario
+
 class Interfact(object):
     bbdd = dbapi.connect("bbdd.dat")
     def __init__(self):
         print("Clase menu cargado")
 
-    def insertUsuario(self, *campos, **datos):
-        print("Insertar")
-        self.bbdd.cursor()
-        self.bbdd.execute("""insert into usuario(""" + campos + """
-         values ('""" + datos + """') """)
-        self.bbdd.commit()
-        self.bbdd.close()
 
+    def insertUsuario(self):
+        Usuario.dni = input((print("Introduce dni: ")))
+        Usuario.nombre = input((print("Introduce nombre: ")))
+        Usuario.tlfn = input((print("Introduce teléfono: ")))
+        Usuario.mail = input((print("Introduce mail: ")))
+        print(Usuario.dni +', '+ Usuario.nombre + ', ' + Usuario.tlfn + ', ' + Usuario.mail)
+#         self.bbdd.cursor()
+#         self.bbdd.execute("""insert into usuario(""" + campos + """
+#          values ('""" + datos + """') """)
+#         self.bbdd.commit()
+#         self.bbdd.close()
+
+    @buscarUsuario
     def buscarUsuario(self):
         print("Buscar")
+
+    @borrarUsuario
     def borrarUsuario(self):
         print("Borrar")
+
+    @listarUsuario
     def listarUsuario(self):
         print("Lista")
 
--- u7/Usuarios/__init__.py
+++ u7/Usuarios/__init__.py
@@ -5,7 +5,6 @@ Created on 24/07/2014
 '''
 import sys
 import Interfact
-import Usuario
 
 salir = True;
 while salir:
@@ -13,24 +12,21 @@ while salir:
     print(" Inserta(i) \n Busca(b) \n Borra(d) \n Lista(l) \n Salir(s)")
     menu = input("i,b,d,l,s: ")
     if (menu == "i"):
-        Interfact.insertUsuario()
+        @Interfact
+        insertUsuario()
     elif(menu == "b"):
-        Interfact.buscarUsuario()
+        @Interfact
+        buscarUsuario()
     elif(menu == "d"):
-        Interfact.borrarUsuario()
+        @Interfact
+        borrarUsuario()
     elif(menu == "l"):
-        Interfact.listarUsuario()
+        @Interfact
+        listarUsuario()
     elif(menu == "s"):
         salir = False
     else:
         print("Operacion desconocida")
-
-Usuario.nombre = "Antonio"
-print(Usuario.nombre)
-Usuario.dni = '16548830N'
-print(Usuario.dni)
-
-Interfact.getMenu()
 
 
 
