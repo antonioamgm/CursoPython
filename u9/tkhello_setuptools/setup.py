c60928e15ffe7b6cb97c85d293f0c1ee6e48380f
antonioam <antonioamgm178@gmail.com>
2014-07-24 19:10:11 +0200

    Revert "Revert "Añadida unidad 9. AAM""
    This reverts commit c9bd7a9d48624e7520aff7a1692bea6f8db314f2.

---
+++ u9/tkhello_setuptools/setup.py
@@ -0,0 +1,12 @@
+﻿from setuptools import setup
+setup(name="Ejemplo Python con Tkinter",
+	version="0.1",
+	description="Ejemplo de ejecución de una aplicación Python con entorno gráfico Tkinter",
+	author="José Luis Merino",
+	author_email="jlmerdiez@gmail.com",
+	url="www.anexpertforyou.com",
+	license="GPL",
+	scripts=["tkhello.py"],
+	platforms=["Win", "Mac", "Linux"],
+	long_description = """Un texto largo iría aquí."""
+)
