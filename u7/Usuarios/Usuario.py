4a0ffd8f27c83d5b0ef54712add221b9a6796925
antonioam <antonioamgm178@gmail.com>
2014-07-24 19:02:46 +0200

    Mas cambios en unidad 7. AAM

--- u7/Usuarios/Usuario.py
+++ u7/Usuarios/Usuario.py
@@ -1,6 +1,37 @@
 class Usuario(object):
-    def __init__(self, nombre, tlfn, mail):
-        self.nombre = nombre
-        self.tlfn = tlfn
-        self.mail = mail
-        print("Usuario %s, telefono %s, mail %s" % (self.nombre, self.tlfn, self.mail))
+    cont = 0
+    __dni = None
+    __nombre = None
+    __tlfn = None
+    __mail = None
+
+    def __init__(self):
+        Usuario.cont +=1
+
+    def setDni(self, dni):
+        self.__dni = dni
+    def getDni(self):
+        return self.__dni
+
+    def setNombre(self, nombre):
+        self.__nombre = nombre
+    def getNombre(self):
+        return self.__nombre
+
+    def setTlfn(self, tlfn):
+        self.__tlfn = tlfn
+    def getTfn(self):
+        return self.__tlfn
+
+    def setMail(self, mail):
+        self.__mail = mail
+    def getMail(self):
+        return self.__mail
+
+
+    dni = property(getDni, setDni)
+    nombre = property(getNombre, setNombre)
+    tlfn = property(getTfn, setTlfn)
+    mail = property(getMail, setMail)
+
+
