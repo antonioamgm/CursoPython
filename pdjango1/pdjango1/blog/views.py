89a500e8f4cf345aa5aed5c18b06b4edde055213
antonioam <antonioamgm178@gmail.com>
2014-07-25 19:34:44 +0200

    Cambios en pdjango

--- pdjango1/pdjango1/blog/views.py
+++ pdjango1/pdjango1/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from blog.models import Entrada
+from django.http import HttpResponse
 
 # Create your views here.
+# def index(request):
+#     # return HttpResponse("Bienvenido al blog")
+#     verentradas = Entrada.objects.all().order_by('-fecha')[:5]
+#     output = ', '.join([p.titulo for p in verentradas])
+#     return HttpResponse(output)
+
+def index(request):
+    entradas = Entrada.objects.all()
+    entrada_string = "Entradas <br/>"
+    entrada_string += '<br/> '.join(["id: %s, titulo: %s"% (e.id, e.titulo) for e in entradas])
+    return HttpResponse(entrada_string)
--- pdjango1/pdjango1/pdjango1/urls.py
+++ pdjango1/pdjango1/pdjango1/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'pdjango1.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^blog/', 'blog.views.index'),
     url(r'^admin/', include(admin.site.urls)),
+
 )
